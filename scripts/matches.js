13ed62e87f69180838eb74947691063d5aafcb67
Jorge Cerdan <jorgecerdanlazaro@gmail.com>
2021-02-10 23:18:08 +0100

    tercer commit, contenido/estilo tablas

--- scripts/matches.js
+++ scripts/matches.js
@@ -1,15 +1,19 @@
 let matches = dataMatches.matches;
 
-function crearTabla(partidos){
+for(let i = 0; i<matches.length; i++){
+    console.log(matches[i].homeTeam.id);
+}
 
+function crearTabla(partidos){
     let tabla = document.getElementById("tabla");
 
     for(let i = 0; i < partidos.length; i++){
         const tr = document.createElement("tr");
 
+        let fecha = new Date(partidos[i].utcDate);
         let fechaPartido = document.createElement("td");
         fechaPartido.className = "fechaPartido";
-        fechaPartido.textContent = partidos[i].utcDate;
+        fechaPartido.textContent = fecha.toLocaleString();
         tr.appendChild(fechaPartido)
 
         let equipoLocal = document.createElement("td");
@@ -17,10 +21,25 @@ function crearTabla(partidos){
         equipoLocal.textContent = partidos[i].homeTeam.name;
         tr.appendChild(equipoLocal)
 
+        let recogidaEscudoLocal = matches[i].homeTeam.id;
+        let recogidaEscudoVisitante = matches[i].awayTeam.id;
+
+        let equipoLocalEscudo = document.createElement("img");
+        equipoLocalEscudo.setAttribute("src", "https://crests.football-data.org/" + recogidaEscudoLocal + ".svg");
+        equipoLocalEscudo.setAttribute("alt", "Escudo equipo");
+        equipoLocalEscudo.classList.add("equipoLocalEscudo");
+        equipoLocal.appendChild(equipoLocalEscudo)
+
         let equipoVisitante = document.createElement("td");
         equipoVisitante.className = "equipoVisitante";
         equipoVisitante.textContent = partidos[i].awayTeam.name;
         tr.appendChild(equipoVisitante)
+
+        let equipoVisitanteEscudo = document.createElement("img");
+        equipoVisitanteEscudo.setAttribute("src", "https://crests.football-data.org/" + recogidaEscudoVisitante + ".svg");
+        equipoVisitanteEscudo.setAttribute("alt", "Escudo equipo");
+        equipoVisitanteEscudo.classList.add("equipoVisitanteEscudo");
+        equipoVisitante.appendChild(equipoVisitanteEscudo)
 
         let resultadoPartido = document.createElement("td");
         resultadoPartido.className = "resultadoPartido";
--- scripts/standings.js
+++ scripts/standings.js
@@ -1,6 +1,8 @@
 let standings = dataStandings.standings;
 let arrayTable = standings[0].table;
 
+// console.log(arrayTable[i].playedGames);
+
 function crearTabla(clasificacionLiga){
     let tabla = document.getElementById("tablaClasificacion");
     
@@ -16,14 +18,48 @@ function crearTabla(clasificacionLiga){
         equipoClasificacionImagen.setAttribute("src", "https://crests.football-data.org/" + clasificacionLiga[0].table[i].team.id + ".svg");
         equipoClasificacionImagen.setAttribute("alt", "Escudo del equipo - Clasificación");
         equipoClasificacionImagen.style.width = "30px";
-        equipoClasificacionImagen.style.margin = "0px auto";
-        tr.appendChild(equipoClasificacion)
-        equipoClasificacion.appendChild(equipoClasificacionImagen)
+        tr.appendChild(equipoClasificacion);
+        equipoClasificacion.appendChild(equipoClasificacionImagen);
 
         let escudoClasificacion = document.createElement("td")
         escudoClasificacion.textContent = clasificacionLiga[0].table[i].team.name;
         tr.appendChild(escudoClasificacion)    
 
+        let partidosJugados = document.createElement("td");
+        partidosJugados.textContent = arrayTable[i].playedGames;
+        tr.appendChild(partidosJugados)
+
+        let partidosGanados = document.createElement("td");
+        partidosGanados.textContent = arrayTable[i].won;
+        tr.appendChild(partidosGanados);
+
+        let partidosEmpatados = document.createElement("td");
+        partidosEmpatados.textContent = arrayTable[i].draw;
+        tr.appendChild(partidosEmpatados);
+
+        let partidosPerdidos = document.createElement("td");
+        partidosPerdidos.textContent = arrayTable[i].lost;
+        tr.appendChild(partidosPerdidos)
+
+        let puntosClasificacion = document.createElement("td");
+        puntosClasificacion.textContent = arrayTable[i].points;
+        tr.appendChild(puntosClasificacion);
+
+        let golesAFavor = document.createElement("td");
+        golesAFavor.textContent = arrayTable[i].goalsFor;
+        tr.appendChild(golesAFavor);
+
+        let golesEnContra = document.createElement("td");
+        golesEnContra.textContent = arrayTable[i].goalsAgainst;
+        tr.appendChild(golesEnContra);
+
+        let diferenciaGoles = document.createElement("td");
+        diferenciaGoles.textContent = arrayTable[i].goalDifference;
+        tr.appendChild(diferenciaGoles);
+
+
+
+        
         tabla.appendChild(tr)
     }
 }
